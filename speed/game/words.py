684c8e5f5cecb1176997341cb730c16046666f58
Jennifer Clark <jennifer.c.schultz@gmail.com>
2021-10-28 16:49:20 -0600

    add file input_servce.py yes I know it's spelled wrong, i will fix that next. also added mor to words.py

--- speed/game/words.py
+++ speed/game/words.py
@@ -1,4 +1,16 @@
 # this needs to pick random words from the words.txt file 
 #these words are those that will cross the screen and the player
-#will have to type them in order to play.
+#will have to copy/type them  as they pass, in order to play.
+import random
 
+class Words:
+    """ This creats a list of words from the words.txt file. It then chooses 5 random words from 
+    that file. """
+
+    def game_words(self):
+        
+
+        with open('words.txt','r') as w:
+            w.strip().split(',')
+            words = random.sample(w, 5)
+        return words     
