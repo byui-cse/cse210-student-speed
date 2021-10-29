1c8050217c0f40f087c28159d5d1e318dc09994c
SysCube <61132193+SysCube@users.noreply.github.com>
2021-10-28 20:11:38 -0500

    DanielCommit

--- speed/game/buffer.py
+++ speed/game/buffer.py
@@ -3,10 +3,19 @@ from game.player import Player
 class Buffer(Player):
     
     def __init__(self):
-        self.buffer = []
+        self._word = ''
+        self._letter = ''
 
     def get_buffer(self):
-        pass
+        return self._word
 
     def set_buffer(self):
-        pass
+        self._word = self._word + self._letter
+    
+    def get_letter(self):
+        return self._letter
+    
+    def set_letter(self):
+        self._letter = self._input_service.get_letter()
+        
+        
