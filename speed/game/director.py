4f4ca2fb0a0c4cccd839f184a9102824973ee244
Jeremy Diamond <socjeremyd@gmail.com>
2021-10-30 16:01:21 -0600

    making these classes resemble the snake game

--- speed/game/director.py
+++ speed/game/director.py
@@ -24,12 +24,15 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-        #self._buffer = Buffer()
+        self._words = []
+        for i in range (constants.STARTING_WORDS):
+            word = Word()
+            self._words.append(word)
         self._input_service = input_service
         self._keep_playing = True
         self._output_service = output_service
         self._score = Score()
-        self._word = Word()
+        self._buffer = Buffer()
 
         
     def start_game(self):
@@ -46,12 +49,14 @@ class Director:
 
     def _get_inputs(self):
         """Gets the inputs at the beginning of each round of play. In this case,
-        that means getting the ...
+        that means getting the adding the letter to the buffer and moving the word to the next spot.
 
         Args:
             self (Director): An instance of Director.
         """
-        pass
+        self._buffer.add_letters(self._input_service.get_letter())
+        for word in self._words:
+            word.move_next()
 
     def _do_updates(self):
         """Updates the important game information for each round of play. In 
@@ -60,7 +65,7 @@ class Director:
         Args:
             self (Director): An instance of Director.
         """
-        pass
+        self._check_word()
         
     def _do_outputs(self):
         """
@@ -70,23 +75,27 @@ class Director:
         """
         
         self._output_service.clear_screen()
-        #self._output_service.draw_actor(self._buffer)
-        self._output_service.draw_actors(self._word.get_all())
+        for word in self._words:
+            self._output_service.draw_actor(word)
+        #self._output_service.draw_actors(self._buffer.get_all())
         self._output_service.draw_actor(self._score)
+        self._output_service.draw_actor(self._buffer.convert_to_string())
         self._output_service.flush_buffer()
 
-    def _(self):
+    def _check_word(self):
         """
+        see if the typed word is on the list of words
 
         Args:
             self (Director): An instance of Director.
         """
-        pass
+        attempt = self._buffer.convert_to_string()
 
-    def _(self):
-        """
+        for word in self._words:
+            actual_word = word.get_text()
 
-        Args:
-            self (Director): An instance of Director.
-        """
-        pass
+            if attempt == actual_word: 
+                self._score.add_points(word.get_points())
+                word.reset()
+
+   
--- speed/game/word.py
+++ speed/game/word.py
@@ -1,85 +1,53 @@
+import random
 from game import constants
 from game.actor import Actor
 from game.point import Point
-import random
-
 class Word(Actor):
+    """A list of Words. The responsibility of word is to keep track of the list of words and generate new ones as needed. 
 
-    """A list of many actors creating a list of words. Facilitates the points per word, 
-    and the generation of new words.
-
-    Stereotypes:
+    Stereotype:
         Information Holder
-
-    Attributes:
-        _words (list): a list to contain all of our instances of word actors.
-        _points (int): keeping track of points in a given word.
+    Attributes: 
+        _points (integer): The number of points the food is worth.
     """
-   
     def __init__(self):
-        """The class constructor.
+        """The class constructor. Invokes the superclass constructor, set's the 
+        text and moves the food to a random position within the boundary of the 
+        screen.
         
         Args:
-            self (Word): An instance of word.
+            self (Actor): an instance of Actor.
         """
-        # call the superclass()
-        # set all attributes
-        # call _prepare_list()
         super().__init__()
         self.all_words = constants.LIBRARY
-        self._words_list = {}
-
-  
-    def get_all(self):
-        # return all words
-        return self._words_list
-
-    
-    def move_word(self, word, x, y):
-        # direction equal to x,y using the Point class
-        # word = self._words[word]
-        # word.set_velocity equal to direction
-        # word.move_next()
-        pass
-
-    
-    def word_check(self, word):
-        # loop through the range of words
-            # set text equal to self._words at current index
-            # if text.get_text() is equal to word
-                # call the set points function and pass the current word
-                # set the current word to a new random word
-                # return points
-        # return 0
-        pass
-
-   
-    def _add_word(self, text, position, velocity):
-        text = Actor()# word set it equal to Actor()
-        self.get_text = text # set the test of our word equal to text
-        self.get_position = position # set the position of our word to position
-        self.get_velocity = velocity# set the velocity of our word to velocity
-        self._words_list.append(text)    # append word to words
+        self.new_word = random.choice(self.all_words)
+        self._points = len(self.new_word)
+        x = random.randint(1, constants.MAX_X - 2)
+        y = random.randint(1, constants.MAX_Y - 2)
+        self._position =Point(x,y)
+        self._velocity = Point(1,0)
         
+    
+    def get_points(self):
+        """Gets the points this word is worth.
+        
+        Args:
+            self (Word): an instance of Word.
+        Returns:
+            integer: The points this Word is worth.
+        """
+        return self._points
 
-  
-    def _prepare_list(self):
-       
-            
-            
-            
-            
-            
-            
-        for i in range(constants.STARTING_WORDS):                       # for range of the constant STARTING_WORDS
-            random_word = self.all_words[random.randint(0,10000)]       # set text equal to a random word from constant LIBRARY
-            x = random.randint(2, constants.MAX_X)                      # set x equal to half of the constant MAX X 
-            y = random.randint(2, constants.MAX_Y)                      # set y equal to half of the constant MAX Y
-            position = self.set_position(x,y)                                      # set position equal to x, y - current index using the Point class
-            velocity = Point(1,0)                                       # set velocity equal to 1,0 using Point class
-
-            self._add_word(random_word,position, velocity )             # call the add_word function and pass, text, position, velocity
-
-    def _set_points(self, word):
-        # set _points equal to the length of word
-        pass
+    def reset(self):
+        """Resets the words by moving it to a random position within the boundaries of the screen and reassigning the points to a random number.
+        
+        Args:
+            self (Word): an instance of Word.
+        """
+        self.all_words = constants.LIBRARY
+        self.new_word = random.choice(self.all_words)
+        self._points = len(self.new_word)
+        x = random.randint(1, constants.MAX_X - 2)
+        y = random.randint(1, constants.MAX_Y - 2)
+        self._position =Point(x,y)
+        self._velocity = Point(1,0)
